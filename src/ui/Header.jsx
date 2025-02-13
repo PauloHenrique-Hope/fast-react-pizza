8c382c73c5fbf568eefd831442a1aaee80b53e6a
unknown <paulo.henriqq8@gmail.com>
2025-02-13 15:53:16 -0400

    Added Header Styles

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza</Link>
-      <p>Satoko</p>
+    <header className="flex text-gray-100 bg-red-500 p-12">
+      <Link className="text-6xl font-bold " to="/">
+        Fast React Pizza
+      </Link>
+      <p className="text-xl font-semibold underline">Satoko</p>
     </header>
   );
 }
